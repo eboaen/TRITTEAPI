5cacee8b6cc514203b55d1f05210bc7e2ac7b32d
Eric Boaen <eric@boaen.org>
2019-11-05 11:45:54 -0500

    New Conventions Page

--- app.py
+++ app.py
@@ -253,9 +253,10 @@ def conventions():
             'tteconvention_id' : tteconvention_id,
             'tteconvention_info' : tteconvention_info
             })
-    return render_template('conventions.html', **{'name' : name,
-    'tteconventions' : tteconventions,
-    })
+    else:
+        return render_template('conventions.html', **{'name' : name,
+        'tteconventions' : tteconventions,
+        })
 # -----------------------------------------------------------------------
 # Run Program
 # -----------------------------------------------------------------------
