9f9d82fc4a57069980492789818fc2a671f03c01
Eric Boaen <eric@boaen.org>
2019-11-07 13:21:48 -0500

    Volunteer Management

--- app.py
+++ app.py
@@ -387,14 +387,14 @@ def conventions():
     if request.method == "POST":
         # Pull all the data regarding the convention
         if request.form.get('consubmit'):
-            tteconvention_id = request.form.get('selectcon',None)
+            session['tteconvention_id'] = request.form.get('selectcon',None)
             print(tteconvention_id)
             tteconvention_data = tte_convention_api_pull(ttesession,tteconvention_id)
             return render_template('conventions.html', conform=conform, fileform=fileform, **{'name' : name,
             'tteconventions' : tteconventions,
             'tteconvention_data' : tteconvention_data
             })
-        if tteconvention_id != None:
+        if session['tteconvention_id'] != None:
             # Volunteer Management
             if request.form.get('volunteersave'):
                 select = request.form.get('selectfile')
