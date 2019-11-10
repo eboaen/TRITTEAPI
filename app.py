43a2faefbf3f30e93d1edc3e5f44fabb8832bff4
Eric Boaen <eric@boaen.org>
2019-11-10 15:24:45 -0500

    Slot Management

--- app.py
+++ app.py
@@ -358,7 +358,6 @@ def slot_save(slots_info,tteconvention_id,tteconvention_name):
     new_slot = {}
     # Check the database to see if the slot already exists for the convention
     for field in slots_info:
-        print(field, slots_info[field])
         if 'slot' in field:
             slot_num = field.rsplit()
             new_slot[slot_num[1]] = slots_info[field]
