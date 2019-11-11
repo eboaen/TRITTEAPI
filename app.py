c8e831e0da681c4f5e3047ffd695e41f48d75f64
Eric Boaen <eric@boaen.org>
2019-11-10 19:53:55 -0500

    Slots

--- app.py
+++ app.py
@@ -325,6 +325,7 @@ def tte_user_add(ttesession,volunteer_email,volunteer_name,tteconvention_id):
 def list_slots(tteconvention_id):
     convention = Conventions()
     convention = Conventions.query.filter_by(tteid = tteconvention_id).first()
+    slots = {}
     if convention.slots is not None:
         con_slots = json.loads(convention.slots)
         for slot in con_slots:
