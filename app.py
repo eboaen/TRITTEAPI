e10cbef01806e5cde44cd5f0e4e87fbc3d016e66
Eric Boaen <eric@boaen.org>
2019-11-11 13:46:16 -0500

    Slots

--- app.py
+++ app.py
@@ -483,14 +483,12 @@ def index():
     if 'name' in session:
         name = session.get('name')
         ttesession = session.get('ttesession')
-        if request.method == 'DELETE':
-            print(request.form.get('logoutsubmit'))
-            if request.form.get('logoutsubmit'):
-                session.pop('name')
-                delete_session_params = {'session_id': session.get('ttesession')}
-                delete_session = requests.delete('https://tabletop.events/api/session/' + ttesession, params= delete_session_params)
-                session.pop('ttesession')
-                return redirect('base.html')
+        if request.form.get('logoutsubmit'):
+            session.pop('name')
+            delete_session_params = {'session_id': session.get('ttesession')}
+            delete_session = requests.delete('https://tabletop.events/api/session/' + ttesession, params= delete_session_params)
+            session.pop('ttesession')
+            return redirect('base.html')
         else:
             return render_template('base.html', logout = logout, **{'name' : name})
     else:
