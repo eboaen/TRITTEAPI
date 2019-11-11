9e92dadaf8015ce035c867f250fe04d7a8f27ba8
Eric Boaen <eric@boaen.org>
2019-11-11 08:47:14 -0500

    Slots

--- app.py
+++ app.py
@@ -409,8 +409,9 @@ def tte_convention_volunteer_shift_api_post(ttesession,tteconvention_id,savedslo
         slot_end = slot_start + datetime.timedelta(hours=slot_length)
         for day in day_info:
             print(datetime.date(slot_start.year,slot_start.month,slot_start.day), datetime.date(day_info[day]['day_time'].year,day_info[day]['day_time'].month,day_info[day]['day_time'].day))
-            #if datetime.date(slot_start.year,slot_start.month,slot_start.day) is datetime.date(day_info[day]['day_time'].year,day_info[day]['day_time'].month,day_info[day]['day_time'].day):
-                    #day_id = day_info[day]['id']
+            if datetime.date(slot_start.year,slot_start.month,slot_start.day) is datetime.date(day_info[day]['day_time'].year,day_info[day]['day_time'].month,day_info[day]['day_time'].day):
+                day_id = day_info[day]['id']
+                print (day_id)
         #print (day_id,slot_start,slot_end)
          # API Post to TTE for Volunteer Shifts
 #        shift_params = {'session_id': ttesession, 'convention_id': tteconvention_id, 'name': slot_name, 'quantity_of_volunteers': '255', 'start_time': slot_start, 'end_time': slot_end, 'conventionday_id': slot_day}
