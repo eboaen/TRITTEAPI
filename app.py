d7ff3f839e4627d61776ec440843b1af26abd0ce
Eric Boaen <eric@boaen.org>
2019-11-11 08:33:47 -0500

    Slots

--- app.py
+++ app.py
@@ -399,7 +399,7 @@ def tte_convention_volunteer_shift_api_post(ttesession,tteconvention_id,savedslo
     day_data = day_response.json()
     for item in day_data['result']['items']:
         day_time = datetime.datetime.strptime(item['start_date'], '%Y/%m/%d %H:%M:%S')
-        day_info[item['name']] = {'id' : item['id'], 'day_time' = day_time}
+        day_info[item['name']] = {'id' : item['id'], 'day_time' : day_time}
     print(day_info)
     for slot in savedslots:
         slot_name = 'Slot' + str(slot)
