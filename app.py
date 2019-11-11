a5d092656c2490e00f19509abf981b84c74c5b00
Eric Boaen <eric@boaen.org>
2019-11-11 17:29:46 -0500

    Volunteers

--- app.py
+++ app.py
@@ -323,8 +323,10 @@ def tte_user_add(ttesession,volunteer_email,volunteer_name,tteconvention_id):
     useradd_params = {'session_id': ttesession['id'],'convention_id' : tteconvention_id,'email_address' : volunteer_email,'firstname' : volunteer_name[0],'lastname' : volunteer_name[1],'phone_number' : '555-555-5555','user_id' : volunteer_email}
     volunteer_response = requests.post('https://tabletop.events/api/volunteer/by-organizer', params= useradd_params)
     volunteer_data = volunteer_response.json()
-    volunteer_id = volunteer_data['result']['items'][0]['id']
-    print(volunteer_email,volunteer_data)
+    try:
+        volunteer_id = volunteer_data['result']['items'][0]['id']
+    except:
+        print(volunteer_email,volunteer_data)
     return(volunteer_data)
 
 # -----------------------------------------------------------------------
