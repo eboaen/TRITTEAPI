ccc5a40bbfb2e12ac422820b283ad1364c9bf750
Eric Boaen <eric@boaen.org>
2019-11-12 15:24:55 -0500

    Events

--- app.py
+++ app.py
@@ -573,7 +573,7 @@ def tte_convention_events_api_post(ttesession,tteconvention_id,savedevents):
 def tte_convention_preferreddaypart_id_api_get(ttesession,tteconvention_id,dayparts_url):
     day_parts_start = 1
     day_parts_total = 100
-    all_dayparts = []
+    all_dayparts = list()
 
     while day_parts_total >= day_parts_start:
         dayparts_params = {'session_id': ttesession['id'], '_page_number': day_parts_start}
