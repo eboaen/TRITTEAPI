1e75ddb5a12614a9925219fea2370fa76734930a
Eric Boaen <eric@boaen.org>
2019-11-12 19:10:28 -0500

    Events

--- app.py
+++ app.py
@@ -602,7 +602,7 @@ def tte_convention_preferreddaypart_id_api_get(ttesession,tteconvention_id,daypa
         if day_parts_start < day_parts_total:
             day_parts_start = int(dayparts_data['result']['paging']['next_page_number'])
             day_parts_total = int(dayparts_data['result']['paging']['total_pages'])
-        elif day_parts_start = day_parts_total:
+        elif day_parts_start == day_parts_total:
             day_parts_total = int(dayparts_data['result']['paging']['total_pages'])
             day_parts_start = day_parts_total + 1
         else:
