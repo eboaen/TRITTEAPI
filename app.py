42429560f7bf08a47525b1d93fb5f7fa7f9a6d6d
Eric Boaen <eric@boaen.org>
2019-11-17 11:35:27 -0500

    Bugfixing

--- app.py
+++ app.py
@@ -861,7 +861,7 @@ def tte_convention_spaces_api_post(ttesession,tteconvention_id,convention_info):
         spaces_params = {'session_id': ttesession['id'], 'convention_id': tteconvention_id, 'room_id': convention_room_id, 'name': table_name, 'max_tickets': 6}
         spaces_response = requests.post(config.tte_url + '/space', params= spaces_params)
         spaces_json = spaces_response.json()
-        spaces_data[table] = {spaces_json['result']['id'],spaces_json['result']['name']}
+        spaces_data = {spaces_json['result']['id'],spaces_json['result']['name']}
         print (spaces_data)
         all_spaces.append(spaces_data)
     return (all_spaces)
