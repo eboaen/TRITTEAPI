d2829718f408c7028cda182840e9258fb16e11e1
Eric Boaen <eric@boaen.org>
2019-11-20 11:12:35 -0500

    Bugfixing: tte_convention_events_type_api_post

--- app.py
+++ app.py
@@ -740,8 +740,8 @@ def tte_convention_events_type_api_post(ttesession,tteconvention_id,events_type)
     events_type_params = {'session_id': ttesession['id'], 'convention_id': tteconvention_id, 'name': events_type, 'limit_volunteers': 0, 'max_tickets': 6, 'user_submittable': 0}
     events_type_response = requests.post(config.tte_url + '/api/eventtype', params= events_type_params)
     events_type_data = events_type_response.json()
-    events_type_id = events_type_data['id']
     print (events_type_data)
+    events_type_id = events_type_data['id']
     return(events_type_id)
 
 # -----------------------------------------------------------------------
