0170beab82a9071b3429dfaa32bbe994d6a29e1b
Eric Boaen <eric@boaen.org>
2019-11-28 09:15:58 -0500

    Bugfix

--- app.py
+++ app.py
@@ -260,10 +260,13 @@ def tte_event_slots_api_get(ttesession,tteconvention_id,slots_url):
 # Create a list of tables assigned to the slot
 # -----------------------------------------------------------------------
 def slots_parse(event_slots):
+    table_l= []
+    slot_tables = []
     for slot in event_slots:
-        slot_l = slot['name'].split()
-        slot_table = slot_l[0] + ' ' + slot_l[1]
-        if slot_table not in slot_tables:
+        table_l = slot['name'].split()
+        table_str = slot_l[0] + ' ' + slot_l[1]
+        table_l.append(table_str)
+        if slot_table not in table_l:
             print (slot_table)
             slot_tables.append(slot_table)
     return (slot_tables)
