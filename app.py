0553a989b7f40807835e0d44b85c5b9bd43fcf9b
Eric Boaen <eric@boaen.org>
2019-12-15 07:14:54 -0500

    Bugfixing

--- app.py
+++ app.py
@@ -1467,8 +1467,8 @@ def tte_geolocation_api_post(ttesession,new_convention):
     geolocation_url = '/api/geolocation'
     geolocation_params = {'session_id': ttesession['id'], 'name': new_convention['location']}
     geolocation_response = requests.post('https://tabletop.events', params= geolocation_params)
-    geolocation_json = geolocation_response.json()
-    print (geolocation_json)
+    #geolocation_json = geolocation_response.json()
+    print (geolocation_response)
     geolocation_id = geolocation_json['result']['items']['id']
     return(geolocation_id)
 
