10d0f8f7f631ae41d5a2a3d9343afc8bb9e33978
Eric Boaen <eric@boaen.org>
2020-01-01 10:55:51 -0500

    Bugfixing

--- app.py
+++ app.py
@@ -831,12 +831,10 @@ def tte_convention_days_api_post(ttesession,tteconvention_id,new_convention):
             'day_type': 'events'
         }
         day_response = requests.post(tteconvention_days_url, params= day_params)
-        print (day_response.url)
-        print (day_params)
         day_json = day_response.json()
         print (day_json)
-        current_day = day_json['id'],day_json['name']
-        all_days.append(day_data)
+        current_day[day_json['result']['name']] = day_json['result']['id'],
+        all_days.append(current_day)
     print (all_days)
     return()
 
