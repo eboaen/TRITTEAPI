79409140566a2144ec9456695844a3a0886a0441
Eric Boaen <eric@boaen.org>
2020-01-05 10:19:06 -0500

    Bugfixing

--- app.py
+++ app.py
@@ -1298,16 +1298,18 @@ def tte_convention_events_api_post(ttesession,tteconvention_id,savedevents):
         else:
             event_type_l = [type for type in event_types if type['name'] == event['type']]
         # If there are event types and a match is found, assign the id of the match to the event
-        if len(event_type_l) !=0:
+        if len(event_type_l) !=0 and event['type'] in event_type_l:
             all_rooms = tte_convention_rooms_api_get(ttesession,tteconvention_id)
-            for e in event_type_l:
-                if e['name'] == event['type']:
-                    event['type_id'] = e['id']
-            for room in all_rooms:
-                if event['type'] == room['name']:
-                    event['type_room_id'] = room['id']
+                for e in event_type_l:
+                    if e['name'] == event['type']:
+                        event['type_id'] = e['id']
+                        print (e['name'], 'Event Type ID: ', event['type_id'])
+                for room in all_rooms:
+                    if event['type'] == room['name']:
+                        event['type_room_id'] = room['id']
+                        print (room['name'], 'Event Room Type ID: ', event['type_room_id'])
+        # Otherwise, create a new Event Type and return the TTE id for that Type, and create an Event Room Type ID.
         else:
-            # If there isn't a match, create a new Event Type and return the TTE id for that Type
             if event['tier'] !='':
                 print ('Adding Event Type to TTE: ', event['type'], event['tier'])
             else:
@@ -1321,7 +1323,6 @@ def tte_convention_events_api_post(ttesession,tteconvention_id,savedevents):
         event['unconverted_datetime'] = datetime.datetime.strptime(event['datetime'],'%m/%d/%y %I:%M:%S %p')
         #Convert the datetime value to UTC
         event['datetime_utc'] = datetime_utc_convert(ttesession,tteconvention_id,event['unconverted_datetime'])
-        print (event['unconverted_datetime'],event['datetime_utc'])
         # Identify the Day Id for the convention
         for day in convention_days:
             day['date_check'] = datetime.date(day['day_time'].year,day['day_time'].month,day['day_time'].day)
