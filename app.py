7ae8c76643de1f6857fb38c56d423eb70f63fc99
Eric Boaen <eric@boaen.org>
2020-01-05 14:57:37 -0500

    Bugfixing

--- app.py
+++ app.py
@@ -1420,9 +1420,9 @@ def tte_event_api_post(ttesession,tteconvention_id,event):
     print ('testing tte_event_api_post')
     if event['tier'] != '':
         event_tier = {
-        'custom_fields': [{
+        'custom_fields': {
         'tier': event['tier']
-        }]
+        }
         }
         event_params = {'session_id': ttesession['id'], 'convention_id': tteconvention_id, 'name' : event['name'], 'max_tickets' : 6, 'priority' : 3, 'age_range': 'all', 'type_id' : event['type_id'], 'conventionday_id': event['day_id'], 'duration' : event['duration'], 'alternatedaypart_id' : event['dayparts_start_id'], 'preferreddaypart_id' : event['dayparts_start_id']}
         event_response = requests.post('https://tabletop.events/api/event', json=event_tier, params= event_params)
