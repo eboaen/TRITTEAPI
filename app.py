49358c4280266ff1d22eef9969c8fae14509afd8
Eric Boaen <eric@boaen.org>
2020-01-11 13:24:38 -0500

    Bugfixing

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ def create_volunteer_report(ttesession,tteconvention_id):
     for volunteer in tteconvention_data['volunteers']:
         for event in tteconvention_data['events']:
             for host in event['hosts']:
-                if volunteer['user_id'] == host['user_id']
+                if volunteer['user_id'] == host['user_id']:
                     volunteer_events = []
                     volunteer_events.append(event)
                     volunteer['events'] = volunteer_events
