4e72dc0a15a6207e6ccf0d331c338363fe8f132f
eboaen <eric@boaen.org>
2020-02-20 07:42:10 -0500

    - Refresh volunteer list after a volunteer is added

--- app.py
+++ app.py
@@ -2081,7 +2081,8 @@ def conventions():
             tteconvention_id = session.get('tteconvention_id')
             tteconvention_name = tteconvention_data['result']['name']
             volunteer_id = request.form.get('volunteer_id')
-            tte_convention_volunteer_api_delete(ttesession,tteconvention_id,volunteer_id)
+            global tteconvention_data
+            tteconvention_data['volunteers'] = tte_convention_volunteer_api_get(ttesession,tteconvention_id)
             tte_convention_api_get(ttesession,session['tteconvention_id'])
             updateconform = conform_info()
             return render_template('conventions.html',  updateconform=updateconform, conform=conform, fileform=fileform, **{'name' : name,
